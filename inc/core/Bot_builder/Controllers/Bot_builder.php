f8f320d6e817cb835c28139d88a332d1a0b56d80
Lonardo Netto <lonardonetto@gmail.com>
2026-07-17 11:50:53 -0300

    debug: add log in send_whatsapp to trace Cloud API sending

--- inc/core/Bot_builder/Controllers/Bot_builder.php
+++ inc/core/Bot_builder/Controllers/Bot_builder.php
@@ -3319,6 +3319,12 @@ private function check_autorespond_delay($bot_id, $phone, $delay_seconds)
             return false;
         }
 
+        // Debug log
+        file_put_contents(WRITEPATH . 'bot_builder_send.log',
+            date('Y-m-d H:i:s') . ' | SEND_WHATSAPP | instance=' . $instance_id . ' | phone=' . $phone . ' | type=' . $type . ' | access_token=' . substr($access_token, 0, 8) . "...\n",
+            FILE_APPEND
+        );
+
         // Redireciona contas Whatsmeow para gateway Go (independente)
         static $whatsmeowGatewayChecked = [];
         if (!isset($whatsmeowGatewayChecked[$instance_id])) {
